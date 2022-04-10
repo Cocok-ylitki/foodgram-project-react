2e3c8b2ca5a119caef6fc11381a5f2e824e997ed
Cocok-ylitki <86740068+Cocok-ylitki@users.noreply.github.com>
2022-04-10 15:59:05 +0500

    try24

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,42 +1,57 @@
+from django_filters import AllValuesMultipleFilter
 from django_filters import rest_framework as filters
+from django_filters.widgets import BooleanWidget
 from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe, Tag
-from users.models import User
+from recipes.models import Recipe
+# Tag
+# from users.models import User
 
 
 class IngredientSearchFilter(SearchFilter):
     search_param = 'name'
 
 
+# class RecipeFilter(filters.FilterSet):
+#     tags = filters.ModelMultipleChoiceFilter(
+#         field_name='tags__slug',
+#         to_field_name='slug',
+#         queryset=Tag.objects.all()
+#     )
+#     author = filters.ModelMultipleChoiceFilter(
+#         field_name='author__id',
+#         to_field_name='id',
+#         queryset=User.objects.all()
+#     )
+#     is_favorited = filters.BooleanFilter(method='filter_favorited_shop_cart')
+#     is_in_shopping_cart = filters.BooleanFilter(
+#         method='filter_favorited_shop_cart'
+#     )
+
+#     class Meta:
+#         model = Recipe
+#         fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
+
+#     def _get_params(self, key):
+#         return {f'{key}__user': self.request.user}
+
+#     def filter_favorited_shop_cart(self, queryset, name, value):
+#         params = {
+#             'is_favorited': self._get_params('favorites'),
+#             'is_in_shopping_cart': self._get_params('shopping_cart')
+#         }
+#         if value:
+#             return queryset.filter(**params[name])
+#         return queryset.exclude(**params[name])
+
+
 class RecipeFilter(filters.FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all()
-    )
-    author = filters.ModelMultipleChoiceFilter(
-        field_name='author__id',
-        to_field_name='id',
-        queryset=User.objects.all()
-    )
-    is_favorited = filters.BooleanFilter(method='filter_favorited_shop_cart')
-    is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_favorited_shop_cart'
-    )
+    is_in_shopping_cart = filters.BooleanFilter(widget=BooleanWidget())
+    is_favorited = filters.BooleanFilter(widget=BooleanWidget())
+    tags = AllValuesMultipleFilter(field_name='tags__slug')
+    author = AllValuesMultipleFilter(field_name='author__id')
 
     class Meta:
         model = Recipe
-        fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
-
-    def _get_params(self, key):
-        return {f'{key}__user': self.request.user}
-
-    def filter_favorited_shop_cart(self, queryset, name, value):
-        params = {
-            'is_favorited': self._get_params('favorites'),
-            'is_in_shopping_cart': self._get_params('shopping_cart')
-        }
-        if value:
-            return queryset.filter(**params[name])
-        return queryset.exclude(**params[name])
+        fields = ['author__id', 'tags__slug',
+                  'is_favorited', 'is_in_shopping_cart']
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-from django.db.models import Sum
+from django.db.models import Sum, BooleanField, Exists, OuterRef, Value
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
@@ -91,7 +91,7 @@ class FollowViewSet(UserViewSet):
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     pagination_class = LimitPageNumberPagination
-    filter_class = (RecipeFilter)
+    filter_class = RecipeFilter
     permission_classes = (AdminUserOrReadOnly,)
 
     def get_serializer_class(self):
@@ -101,6 +101,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Recipe.objects.all()
+
+        if user.is_authenticated:
+            queryset = queryset.annotate(
+                is_favorited=Exists(Favorite.objects.filter(
+                    user=user, recipe__pk=OuterRef('pk'))
+                ),
+                is_in_shopping_cart=Exists(Cart.objects.filter(
+                    user=user, recipe__pk=OuterRef('pk'))
+                )
+            )
+        else:
+            queryset = queryset.annotate(
+                is_favorited=Value(False, output_field=BooleanField()),
+                is_in_shopping_cart=Value(False, output_field=BooleanField())
+            )
+        return
 
     @action(detail=True, methods=['post'],
             permission_classes=(IsAuthenticated,))
