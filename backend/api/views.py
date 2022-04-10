85c464aa6dbca4ad5b7eeed7d8a1f6f92b31d549
Cocok-ylitki <86740068+Cocok-ylitki@users.noreply.github.com>
2022-04-10 16:54:14 +0500

    try25

--- backend/api/views.py
+++ backend/api/views.py
@@ -159,19 +159,48 @@ class RecipeViewSet(viewsets.ModelViewSet):
             'errors': 'Ошибка удаления рецепта из списка'
         }, status=HTTPStatus.BAD_REQUEST)
 
+    # @action(
+    #     detail=False, methods=['get'], permission_classes=(IsAuthenticated,))
+    # def download_shopping_cart(self, request):
+    #     ingredients = IngredientQuantity.objects.filter(
+    #         recipe__shopping_cart__user=request.user).values(
+    #         'ingredients__name',
+    #         'ingredients__measurement_unit').annotate(total=Sum('amount'))
+    #     shopping_cart = '\n'.join([
+    #         f'{ingredient["ingredient__name"]} - {ingredient["total"]} '
+    #         f'{ingredient["ingredient__measurement_unit"]}'
+    #         for ingredient in ingredients
+    #     ])
+    #     filename = 'shopping_cart.txt'
+    #     response = HttpResponse(shopping_cart, content_type='text/plain')
+    #     response['Content-Disposition'] = f'attachment; filename={filename}'
+    #     return response
     @action(
-        detail=False, methods=['get'], permission_classes=(IsAuthenticated,))
+        detail=False, methods=['get'], permission_classes=[IsAuthenticated])
     def download_shopping_cart(self, request):
-        ingredients = IngredientQuantity.objects.filter(
-            recipe__shopping_cart__user=request.user).values(
-            'ingredients__name',
-            'ingredients__measurement_unit').annotate(total=Sum('amount'))
-        shopping_cart = '\n'.join([
-            f'{ingredient["ingredient__name"]} - {ingredient["total"]} '
-            f'{ingredient["ingredient__measurement_unit"]}'
-            for ingredient in ingredients
-        ])
+        user = get_object_or_404(User, username=request.user.username)
+        shopping_cart = user.cart.all()
+        shopping_dict = {}
+        for num in shopping_cart:
+            ingredients_queryset = num.recipe.ingredient.all()
+            for ingredient in ingredients_queryset:
+                name = ingredient.ingredients.name
+                amount = ingredient.amount
+                measurement_unit = ingredient.ingredients.measurement_unit
+                if name not in shopping_dict:
+                    shopping_dict[name] = {
+                        'measurement_unit': measurement_unit,
+                        'amount': amount}
+                else:
+                    shopping_dict[name]['amount'] = (
+                        shopping_dict[name]['amount'] + amount)
+
+        shopping_list = []
+        for index, key in enumerate(shopping_dict, start=1):
+            shopping_list.append(
+                f'{index}. {key} - {shopping_dict[key]["amount"]} '
+                f'{shopping_dict[key]["measurement_unit"]}\n')
         filename = 'shopping_cart.txt'
-        response = HttpResponse(shopping_cart, content_type='text/plain')
+        response = HttpResponse(shopping_list, content_type='text/plain')
         response['Content-Disposition'] = f'attachment; filename={filename}'
         return response
