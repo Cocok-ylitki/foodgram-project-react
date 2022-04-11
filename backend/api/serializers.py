b2f620bbc91c49d6c3d92b235676391eedcfbad9
Cocok-ylitki <86740068+Cocok-ylitki@users.noreply.github.com>
2022-04-12 00:47:58 +0500

    try25

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -91,10 +91,10 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
         ingredients = self.initial_data.get('ingredients')
         ingredients_set = set()
         for ingredient in ingredients:
-            if type(ingredient.get('amount')) is str:
+            if type(ingredient.get('amount')) == str:
                 if not ingredient.get('amount').isdigit():
                     raise serializers.ValidationError(
-                        ('Количество ингредиента должно быть числом')
+                        ('Количество ингредиента дольжно быть числом')
                     )
             if int(ingredient.get('amount')) <= 0:
                 raise serializers.ValidationError(
