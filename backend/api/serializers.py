9d17dc92b831a126b730da7063214f9f20543f79
Cocok-ylitki <86740068+Cocok-ylitki@users.noreply.github.com>
2022-04-12 00:39:47 +0500

    try25

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -88,7 +88,7 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
         )
 
     def validate(self, data):
-        ingredients = data['ingredients']
+        ingredients = data.get('ingredients')
         ingredients_set = set()
         for ingredient in ingredients:
             if type(ingredient.get('amount')) is str:
