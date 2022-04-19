b0752d5ceaedb8235a2e898470a059cb8052b634
Cocok-ylitki <86740068+Cocok-ylitki@users.noreply.github.com>
2022-04-20 01:07:27 +0500

    try44

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# praktikum_new_diplom
+# Проект Foodgram «Продуктовый помощник»
+![push](https://github.com/Cocok-ylitki/foodgram-project-react/actions/workflows/workflow.yml/badge.svg?event=push)
+
+http://51.250.97.31/
--- backend/api/filters.py
+++ backend/api/filters.py
@@ -12,39 +12,6 @@ class IngredientSearchFilter(SearchFilter):
     search_param = 'name'
 
 
-# class RecipeFilter(filters.FilterSet):
-#     tags = filters.ModelMultipleChoiceFilter(
-#         field_name='tags__slug',
-#         to_field_name='slug',
-#         queryset=Tag.objects.all()
-#     )
-#     author = filters.ModelMultipleChoiceFilter(
-#         field_name='author__id',
-#         to_field_name='id',
-#         queryset=User.objects.all()
-#     )
-#     is_favorited = filters.BooleanFilter(method='filter_favorited_shop_cart')
-#     is_in_shopping_cart = filters.BooleanFilter(
-#         method='filter_favorited_shop_cart'
-#     )
-
-#     class Meta:
-#         model = Recipe
-#         fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
-
-#     def _get_params(self, key):
-#         return {f'{key}__user': self.request.user}
-
-#     def filter_favorited_shop_cart(self, queryset, name, value):
-#         params = {
-#             'is_favorited': self._get_params('favorites'),
-#             'is_in_shopping_cart': self._get_params('shopping_cart')
-#         }
-#         if value:
-#             return queryset.filter(**params[name])
-#         return queryset.exclude(**params[name])
-
-
 class RecipeFilter(filters.FilterSet):
     is_in_shopping_cart = filters.BooleanFilter(widget=BooleanWidget())
     is_favorited = filters.BooleanFilter(widget=BooleanWidget())
